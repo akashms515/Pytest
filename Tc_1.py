89bd471fcf3efb8f9ee578d59ec934e7db59293d
akashms515 <146930936+akashms515@users.noreply.github.com>
2025-02-14 21:53:57 +0530

    Tc_1.py

---
+++ Tc_1.py
@@ -0,0 +1,30 @@
+import os
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+driver_path = r"C:\Users\Raja\Desktop\chromedriver.exe"
+os.environ["PATH"] += os.pathsep + os.path.dirname(driver_path)
+
+chrome_options = Options()
+
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+driver.maximize_window()
+time.sleep(2)
+username = driver.find_element(By.XPATH,'/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input')
+username.click()
+time.sleep(2)
+username.send_keys("Admin")
+time.sleep(2)
+passw=driver.find_element(By.XPATH,'/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input')
+passw.click()
+time.sleep(2)
+passw.send_keys("admin123")
+time.sleep(2)
+logc= driver.find_element(By.XPATH,'/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button')
+logc.click()
+driver.quit()
